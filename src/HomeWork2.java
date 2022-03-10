7d08bb95528ed2cfbefbea113473d625a52fb71a
Angel <86420291+malelelonely@users.noreply.github.com>
2022-03-10 13:46:19 +0200

    First HomeWork code academy

--- src/HomeWork2.java
+++ src/HomeWork2.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class HomeWork2 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter two digit :");//
+        System.out.println("Enter two digit :");
         System.out.println();
         System.out.println("First digit : ");
         int a = scanner.nextInt();
--- src/HomeWork5.java
+++ src/HomeWork5.java
@@ -1,8 +1,7 @@
-import java.util.Scanner;
 
 public class HomeWork5 {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+
         System.out.println("Original digit : \n-20 , 92 , 638 , 73 , 200 , -22");
         System.out.println("Digit raised to the second power");
         System.out.print(Math.pow(-20,2)+" ");
@@ -11,5 +10,5 @@ public class HomeWork5 {
         System.out.print(Math.pow(73,2)+" ");
         System.out.print(Math.pow(200,2)+" ");
         System.out.print(Math.pow(-22,2)+" ");
-    }//
+    }
 }
