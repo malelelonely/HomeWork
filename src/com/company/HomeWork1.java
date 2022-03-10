7bbeee3b1462fee8b1f471fdf00dc8ff96427973
Angel <86420291+malelelonely@users.noreply.github.com>
2022-03-10 12:31:44 +0200

    First HomeWork code academy1

--- src/com/company/HomeWork1.java
+++ src/com/company/HomeWork1.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 public class HomeWork1 {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);//
         System.out.println("Enter your name : ");
         String text = scanner.nextLine();
 
