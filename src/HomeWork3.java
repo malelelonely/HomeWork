e2761470bd316b4e4fb606f1ca0bcf46597a0c17
Angel <86420291+malelelonely@users.noreply.github.com>
2022-03-10 13:53:17 +0200

    First HomeWork code academy

--- src/HomeWork3.java
+++ src/HomeWork3.java
@@ -1,8 +1,9 @@
-import java.util.Scanner;
 
 public class HomeWork3 {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        System.out.println("Original number : -147 , 15 , 0 , 18");
+        System.out.println();
         System.out.print("Absolute value of number is : "+Math.abs(-147)+ " "+Math.abs(15)+ " "+Math.abs(0)+ " "+Math.abs(18));
+
     }
 }
