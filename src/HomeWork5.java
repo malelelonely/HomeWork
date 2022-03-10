a6f64b5a856d7f8bf22272d518f683483806b24b
Angel <86420291+malelelonely@users.noreply.github.com>
2022-03-10 13:54:44 +0200

    First HomeWork code academy

--- src/HomeWork5.java
+++ src/HomeWork5.java
@@ -3,6 +3,7 @@ public class HomeWork5 {
     public static void main(String[] args) {
 
         System.out.println("Original digit : \n-20 , 92 , 638 , 73 , 200 , -22");
+        System.out.println();
         System.out.println("Digit raised to the second power");
         System.out.print(Math.pow(-20,2)+" ");
         System.out.print(Math.pow(92,2)+" ");
@@ -10,5 +11,6 @@ public class HomeWork5 {
         System.out.print(Math.pow(73,2)+" ");
         System.out.print(Math.pow(200,2)+" ");
         System.out.print(Math.pow(-22,2)+" ");
+        System.out.println();
     }
 }
