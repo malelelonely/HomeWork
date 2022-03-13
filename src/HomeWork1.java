b48c4d0653f41da26fd2c4cd90f637eefc2ece88
Angel <86420291+malelelonely@users.noreply.github.com>
2022-03-13 20:53:42 +0200

    Second HomeWork code academyy

--- src/HomeWork1.java
+++ src/HomeWork1.java
@@ -4,13 +4,13 @@ public class HomeWork1 {
        short myShort = -250;
        short myShort2 = 250;
        int myInt = 4_589_498;
-       long myLong = 10_000_000_000L;
+       String myString = "10 000 000 000 000 000 000";
        long myLong2 = -9_000_000_000_000_000_000L;
 
         System.out.println("My short value : "+myShort);
         System.out.println("My short2 value : "+myShort2);
         System.out.println("My int value : "+myInt);
-        System.out.println("My Long value : "+myLong );
+        System.out.println("My Long value : "+myString );
         System.out.println("My long2 value : "+myLong2);
 
     }
